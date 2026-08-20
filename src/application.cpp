6d721eb9a6bd6a5457046cb0d3c341f851ce4a7d
Akarin01 <akarin_01@163.com>
2026-08-20 16:51:06 +0800

    refactor: 实现 Application，封装程序业务逻辑

---
+++ src/application.cpp
@@ -0,0 +1,81 @@
+#include "application.h"
+
+#include "core/window.h"
+#include "core/input.h"
+#include "core/time.h"
+#include "scene/scene.h"
+#include "scene/asset_loader.h"
+#include "scene/camera_controller.h"
+#include "render/renderer.h"
+
+#include <filesystem>
+#include <stdexcept>
+
+namespace Kita::Pbrv
+{
+    namespace
+    {
+        void InitScene(Scene& scene)
+        {
+            scene.GetLight()
+                .SetDirection(glm::vec3(1.0f, 1.0f, 0.0f));
+
+            Kita::Pbrv::Mesh& mesh = scene.GetMesh();
+            Kita::Pbrv::AssetLoader::LoadGltfMesh("assets/models/Cerberus_LP.glb", mesh);
+
+            Kita::Pbrv::Material& mat = scene.GetMaterial();
+            Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_A.tga", TextureType::Albedo, mat.GetAlbedoTex());
+            Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_N.tga", TextureType::Normal, mat.GetNormalTex());
+            Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_M.tga", TextureType::Linear, mat.GetMetallicTex());
+            Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_R.tga", TextureType::Linear, mat.GetRoughnessTex());
+            Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_AO.tga", TextureType::Linear, mat.GetAOTex());
+
+            Kita::Pbrv::Skybox& skybox = scene.GetSkybox();
+            Kita::Pbrv::AssetLoader::LoadSkybox("assets/textures/newport_loft.hdr", skybox);
+        }
+    }
+
+    Application::Application()
+    {
+        // Check if the "assets" directory exists
+        if (!std::filesystem::is_directory("assets"))
+        {
+            throw std::runtime_error("assets/ directory not found");
+        }
+
+        m_window = std::make_unique<Window>(800, 600, "Vk Pbr Viewer");
+        m_input = std::make_unique<Input>(*m_window);
+        m_time = std::make_unique<Time>();
+
+        m_scene = std::make_unique<Scene>();
+        InitScene(*m_scene);
+
+        m_renderer = std::make_unique<Renderer>(*m_window);
+    }
+
+    Application::~Application() = default;
+
+    void Application::Run()
+    {
+        CameraController cameraController(*m_input, m_scene->GetCamera());
+
+        while (!m_window->ShouldClose())
+        {
+            m_window->PollEvents();
+            m_input->Update();
+            m_time->Update();
+
+            float deltaTime = m_time->GetDeltaTime();
+
+            if (m_input->IsKeyPressed(Key::Escape))
+            {
+                m_window->RequestClose();
+                continue;
+            }
+
+            cameraController.Update(deltaTime);
+
+            m_renderer->DrawFrame(*m_scene);
+        }
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,12 @@
-#include <stdexcept>
-#include <filesystem>
-
-#include "core/input.h"
+#include "application.h"
 #include "core/log.h"
-#include "core/time.h"
-#include "core/window.h"
-#include "render/renderer.h"
-#include "scene/scene.h"
-#include "scene/asset_loader.h"
-#include "scene/camera_controller.h"
-
-using TextureType = Kita::Pbrv::TextureType;
-using Key = Kita::Pbrv::Key;
-using MouseButton = Kita::Pbrv::MouseButton;
 
 int main()
 {
     try
     {
-        // Check if the "assets" directory exists
-        if (!std::filesystem::is_directory("assets"))
-        {
-            throw std::runtime_error("assets/ directory not found");
-        }
-
-        Kita::Pbrv::Window window(800, 600, "Vk Pbr Viewer");
-        Kita::Pbrv::Input input(window);
-        Kita::Pbrv::Time time{};
-
-        Kita::Pbrv::Scene scene;
-        scene.GetLight()
-            .SetDirection(glm::vec3(1.0f, 1.0f, 0.0f));
-
-        Kita::Pbrv::Mesh& mesh = scene.GetMesh();
-        Kita::Pbrv::AssetLoader::LoadGltfMesh("assets/models/Cerberus_LP.glb", mesh);
-
-        Kita::Pbrv::Material& mat = scene.GetMaterial();
-        Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_A.tga", TextureType::Albedo, mat.GetAlbedoTex());
-        Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_N.tga", TextureType::Normal, mat.GetNormalTex());
-        Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_M.tga", TextureType::Linear, mat.GetMetallicTex());
-        Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_R.tga", TextureType::Linear, mat.GetRoughnessTex());
-        Kita::Pbrv::AssetLoader::LoadTexture("assets/textures/Cerberus_AO.tga", TextureType::Linear, mat.GetAOTex());
-
-        Kita::Pbrv::CameraController cameraController(input, scene.GetCamera());
-
-        Kita::Pbrv::Renderer renderer(window);
-
-        bool showSkybox = false;
-        while (!window.ShouldClose())
-        {
-            window.PollEvents();
-            input.Update();
-            time.Update();
-
-            float deltaTime = time.GetDeltaTime();
-
-            if (input.IsKeyPressed(Key::Escape))
-            {
-                window.RequestClose();
-                continue;
-            }
-
-            if (input.IsMouseButtonPressed(MouseButton::Left))
-            {
-                showSkybox = !showSkybox;
-                if (showSkybox)
-                {
-                    Kita::Pbrv::AssetLoader::LoadSkybox("assets/textures/newport_loft.hdr", scene.GetSkybox());
-                }
-                else
-                {
-                    scene.GetSkybox().SetEmpty();
-                }
-            }
-
-            cameraController.Update(deltaTime);
-
-            renderer.DrawFrame(scene);
-        }
+        Kita::Pbrv::Application app{};
+        app.Run();
 
         return EXIT_SUCCESS;
     }
