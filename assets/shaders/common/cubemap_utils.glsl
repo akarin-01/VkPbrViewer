3483f93c992c9b409d551800f91836740012fe42
Akarin01 <akarin_01@163.com>
2026-08-24 01:41:56 +0800

    refactor: 提取 cubemap_utils glsl 头文件

---
+++ assets/shaders/common/cubemap_utils.glsl
@@ -0,0 +1,19 @@
+#ifndef CUBEMAP_UTILS
+#define CUBEMAP_UTILS
+
+// Face, faceCoord -> World dir(cube)
+vec3 GetDirection(uint face, vec2 faceCoord)
+{
+    switch (face)
+    {
+    case 0u: return normalize(vec3( 1.0, -faceCoord.y, -faceCoord.x));      // +X
+    case 1u: return normalize(vec3(-1.0, -faceCoord.y,  faceCoord.x));      // -X
+    case 2u: return normalize(vec3( faceCoord.x,  1.0,  faceCoord.y));      // +Y
+    case 3u: return normalize(vec3( faceCoord.x, -1.0, -faceCoord.y));      // -Y
+    case 4u: return normalize(vec3( faceCoord.x, -faceCoord.y,  1.0));      // +Z
+    case 5u: return normalize(vec3(-faceCoord.x, -faceCoord.y, -1.0));      // -Z
+    }
+    return vec3(0.0);
+}
+
+#endif
