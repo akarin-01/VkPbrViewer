6b863fc37c2e644d9b262861a5ce490177d40834
Akarin01 <akarin_01@163.com>
2026-09-04 23:27:17 +0800

    refactor: 默认场景改用 LoadModel 一键加载模型

--- src/application/app.cpp
+++ src/application/app.cpp
@@ -21,14 +21,26 @@ namespace Kita::Pbrv
         {
             void InitScene(Scene::Scene& scene, Resource::AssetManager& assetMgr)
             {
-                scene.CreateObject()
-                    .SetMesh(assetMgr.LoadMesh("assets/models/DamagedHelmet/DamagedHelmet.gltf"))
-                    .GetMaterial()
-                    .SetAlbedoTex(assetMgr.LoadTexture("assets/models/DamagedHelmet/Default_albedo.jpg", Resource::TextureAsset::Type::Srgb))
-                    .SetNormalTex(assetMgr.LoadTexture("assets/models/DamagedHelmet/Default_normal.jpg", Resource::TextureAsset::Type::Normal))
-                    .SetMRTex(assetMgr.LoadTexture("assets/models/DamagedHelmet/Default_metalRoughness.jpg", Resource::TextureAsset::Type::MetallicRoughness))
-                    .SetAOTex(assetMgr.LoadTexture("assets/models/DamagedHelmet/Default_AO.jpg", Resource::TextureAsset::Type::Linear))
-                    .SetEmissiveTex(assetMgr.LoadTexture("assets/models/DamagedHelmet/Default_emissive.jpg", Resource::TextureAsset::Type::Srgb));
+                auto result = assetMgr.LoadModel("assets/models/DamagedHelmet/DamagedHelmet.gltf");
+
+                for (const auto& instance : result.m_instances)
+                {
+                    auto& object = scene.CreateObject();
+                    object.SetName(instance.m_name);
+                    object.SetMesh(instance.m_mesh);
+                    object.SetTransform(instance.m_transform);
+                    object.GetMaterial().SetParams(instance.m_material);
+
+                    for (uint32_t slot = 0; slot < Resource::kMaterialSlotCount; ++slot)
+                    {
+                        const auto& texture = instance.m_textures[slot];
+                        if (texture.IsValid())
+                        {
+                            object.GetMaterial().SetTexture(
+                                static_cast<Resource::MaterialSlot>(slot), texture);
+                        }
+                    }
+                }
 
                 auto& skybox = scene.GetSkybox();
                 skybox.SetSkybox(assetMgr.LoadTexture("assets/hdr/qwantani_moon_noon_puresky_4k.hdr", Resource::TextureAsset::Type::Hdr));
